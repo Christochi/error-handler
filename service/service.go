428d445be483e7e916a8cd067651aa88331f18fe
Christochi <tochukwui2@gmail.com>
2023-12-05 18:26:28 +0000

    switch type assertion and removed comments

--- service/service.go
+++ service/service.go
@@ -1,27 +1,27 @@
 package service
 
 import (
-	"errors"
 	"fmt"
-)
-
-// generic http errors
-var (
-	ErrNotImplemented      = errors.New("not implemented")
-	ErrNotFound            = errors.New("not found")
-	ErrInternalServerError = errors.New("internal server error")
-	ErrUnprocessableEntity = errors.New("syntax error in json")
+	"strconv"
 )
 
 type ServiceError struct {
-	appErr error // error returned by the application, adapter, external package or go lib
-	svcErr error // generic error returned from the service e.g. http, grpc, database, etc
+	appErr  error  // error returned by the application, adapter, external package or go lib
+	message string // error description
 }
 
 func (svc *ServiceError) Error() string {
-	return fmt.Sprintf("error: %v - %v", svc.svcErr, svc.appErr)
+	return fmt.Sprintf("error: %v - %v", svc.message, svc.appErr)
 }
 
-func NewError(appError error, svc error) error {
-	return &ServiceError{appErr: appError, svcErr: svc}
+func NewError(appError error, message any) error {
+
+	switch desc := message.(type) {
+	case int:
+		return &ServiceError{appErr: appError, message: strconv.Itoa(desc)}
+	case error:
+		return &ServiceError{appErr: appError, message: desc.Error()}
+	default:
+		return &ServiceError{appErr: appError, message: desc.(string)}
+	}
 }
