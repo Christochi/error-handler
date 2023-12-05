1e504aebbcbe9a702756ce980fa99bbdeebc6e3c
Christochi <tochukwui2@gmail.com>
2023-12-05 06:19:16 +0000

    made struct private

--- service/service.go
+++ service/service.go
@@ -2,15 +2,15 @@ package service
 
 import "fmt"
 
-type ServiceError struct {
+type serviceError struct {
 	appErr string // description of the error
 	svcErr error  // generic error returned from the service e.g. http, grpc, database, etc
 }
 
-func (svc *ServiceError) Error() string {
+func (svc *serviceError) Error() string {
 	return fmt.Sprintf("error: %v - %v", svc.svcErr, svc.appErr)
 }
 
 func NewError(message string, svc error) error {
-	return &ServiceError{appErr: message, svcErr: fmt.Errorf("service-error type: %w", svc)}
+	return &serviceError{appErr: message, svcErr: fmt.Errorf("service-error type: %w", svc)}
 }
