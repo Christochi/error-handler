93d568780e78ab43789b8182c6cff0c79001fe21
Christochi <tochukwui2@gmail.com>
2023-12-05 06:23:34 +0000

    comment, appErr is error type

--- service/service.go
+++ service/service.go
@@ -3,14 +3,14 @@ package service
 import "fmt"
 
 type serviceError struct {
-	appErr string // description of the error
-	svcErr error  // generic error returned from the service e.g. http, grpc, database, etc
+	appErr error // error returned by the application, external package or go lib
+	svcErr error // generic error returned from the service e.g. http, grpc, database, etc
 }
 
 func (svc *serviceError) Error() string {
 	return fmt.Sprintf("error: %v - %v", svc.svcErr, svc.appErr)
 }
 
-func NewError(message string, svc error) error {
-	return &serviceError{appErr: message, svcErr: fmt.Errorf("service-error type: %w", svc)}
+func NewError(appError error, svc error) error {
+	return &serviceError{appErr: appError, svcErr: svc}
 }
