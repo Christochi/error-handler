6f993980956838e6e167ed91eba2e7e2dc270286
Christochi <tochukwui2@gmail.com>
2023-12-06 06:18:11 +0000

    TestOpenFile()

---
+++ example/example_test.go
@@ -0,0 +1,18 @@
+// Test example
+package example
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/Christochi/error-handler/service"
+)
+
+func TestOpenFile(t *testing.T) {
+	err := openFile()
+
+	var svcErr *service.ServiceError
+	if errors.As(err, &svcErr) {
+		t.Errorf("want %v, got %v", nil, err)
+	}
+}
